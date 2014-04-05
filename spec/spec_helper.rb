83186c52c8890cac30a3050219292cfae54298fa
Tim Riley <tim@openmonkey.com>
2014-04-05 23:15:29 +1100

    Tidy up spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,7 +40,6 @@ require "rspec/rails"
 Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
-# If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
