74eb848af06da47fc73235e746582a08f21a3ac6
Tim Riley <tim@openmonkey.com>
2014-04-05 21:04:55 +1100

    Flesh out RSpec configuration
    * Run the non-feature specs (shuffled) before feature specs
    * Use Fuubar output formatter
    * Use new "expect" syntax
    * Use database_cleaner instead of transactional fixtures
    * Use capybara for feature specs
    * Use poltergeist for JS feature specs
    * Mix in factory_girl helpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,10 +25,10 @@ if ENV["COVERAGE"]
   end
 end
 
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
+ENV["RAILS_ENV"] ||= "test"
+
 require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
+require "rspec/rails"
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -44,25 +44,31 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
-  # ## Mock Framework
-  #
-  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
-  #
-  # config.mock_with :mocha
-  # config.mock_with :flexmock
-  # config.mock_with :rr
+  # Require the new expect() syntax.
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  # See database_cleaner.rb for database cleanup details.
+  config.use_transactional_fixtures = false
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
+  # If true, the base class of anonymous controllers will be inferred
+  # automatically. This will be the default behavior in future versions of
+  # rspec-rails.
+  config.infer_base_class_for_anonymous_controllers = true
 
   # Run specs in random order to surface order dependencies. If you find an
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
-  config.order = "random"
+  # config.order = "random"
+
+  # Run non-feature specs (shuffled) before feature specs.
+  config.register_ordering(:global) do |items|
+    features, others = items.partition { |g| g.metadata[:type] == :feature }
+    others.shuffle + features
+  end
 end
+
+# Turn down the logging while testing.
+#Rails.logger.level = 4
