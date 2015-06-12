e33fb58e616e2ed0af4ce2e75abc14b5594863c3
Daniel Nitsikopoulos <daniel.nitsikopoulos@icelab.com.au>
2015-06-12 13:14:54 +1000

    Add RSpec examples that fail if db:seed or db:sample_data raise an error

---
+++ spec/sample_data_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+RSpec.describe 'db:sample_data' do
+  include_context 'rake'
+
+  it 'seeds the database with sample_data - `rake db:sample_data`' do
+    expect { subject.invoke }.to_not raise_error
+  end
+end
+
+RSpec.describe 'db:seed' do
+
+  it 'seeds the database with seed data - `rake db:seed`' do
+    expect { Rails.application.load_seed }.to_not raise_error
+  end
+end
