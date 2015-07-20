7e5b5eafea8cd46e157cef9822e4f39a0bcf25ec
Max Wheeler <max@makenosound.com>
2015-07-20 16:21:38 +1000

    Dasherize the `file_name` for asset targets.

--- lib/generators/asset_target/asset_target_generator.rb
+++ lib/generators/asset_target/asset_target_generator.rb
@@ -1,8 +1,9 @@
 class AssetTargetGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
   def copy_initializer_file
-    copy_file "target.js", "assets/targets/#{file_name}/target.js"
-    copy_file "index.js", "assets/targets/#{file_name}/index.js"
-    copy_file "index.css", "assets/targets/#{file_name}/index.css"
+    target_name = file_name.dasherize
+    copy_file "target.js", "assets/targets/#{target_name}/target.js"
+    copy_file "index.js",  "assets/targets/#{target_name}/index.js"
+    copy_file "index.css", "assets/targets/#{target_name}/index.css"
   end
 end
