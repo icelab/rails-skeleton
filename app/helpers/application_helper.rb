a4d66fa8543d73ecb407de5df2ca891d1b295c5f
Max Wheeler <max@makenosound.com>
2015-07-21 15:36:45 +1000

    Add the `development_javascript` helper for including the webpack hot loader scripts.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
   # Common helpers sit under lib/helpers
   include TextFormattingHelper
+
+  def development_javascript
+    return unless Rails.env.development?
+    javascript_include_tag \
+      "http://#{ENV['ASSETS_DEVELOPMENT_HOST']}:#{ENV['ASSETS_WEBPACK_PORT']}/webpack-dev-server.js",
+      "data-turbolinks-track" => true
+  end
+
 end
