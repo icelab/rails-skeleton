8d28a89669c9e59708fbc9b11e7d1a9d7dbd4d9f
Tim Riley <tim@openmonkey.com>
2015-08-16 08:19:47 +1000

    Add a spec for the development_javascript helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,6 @@
 # Public: General purpose helpers.
 module ApplicationHelper
   def development_javascript
-    return unless Rails.env.development?
-
     javascript_include_tag(
       "http://#{ENV['ASSETS_DEVELOPMENT_HOST']}:#{ENV['ASSETS_WEBPACK_PORT']}/webpack-dev-server.js",
       "data-turbolinks-track" => true)
