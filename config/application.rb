2f4e7efbe04021fffb3c6950113e7e7e9843f68a
Alex Timofeev <atimofeev@reactant.ru>
2016-07-22 12:09:32 +0300

    Prepare config file for `rails-5` to avoid deprecation warnings and possible conflicts

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,9 @@
-require File.expand_path("../boot", __FILE__)
+require_relative 'boot'
 
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
-# you"ve limited to :test, :development, or :production.
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 # Require platform-specific gems.
@@ -11,7 +11,6 @@ platform = RUBY_PLATFORM.match(/(linux|darwin)/)[0].to_sym
 Bundler.require(platform)
 
 module AppPrototype
-  # Public: Main application.
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
